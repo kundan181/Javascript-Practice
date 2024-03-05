7c078c2a31ce991dfaf58a94e971ed8eff5965ed
Kundan Mali <93983780+kundan181@users.noreply.github.com>
2024-03-05 11:53:34 +0530

    Update dataType.js

--- BasicsJS/dataType.js
+++ BasicsJS/dataType.js
@@ -1,1 +1,20 @@
 
+const accountId = 121;
+let name = "Kundan"
+var mailId = "malikundan7709@gmail.com"
+city = "Pune";
+
+//number string boolean undefined null object
+
+
+
+console.log(accountId);
+
+//if we want to print all value in table form we use like this
+
+console.table([accountId,name,mailId,city]);
+
+
+//typeof --> use to know the type of the variable
+
+console.log(typeof "Kundan");
