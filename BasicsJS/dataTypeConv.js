5f4b210888d206f42789676a32d32b89de7f7a40
Kundan Mali <93983780+kundan181@users.noreply.github.com>
2024-03-05 12:17:33 +0530

    Create dataTypeConv.js

---
+++ BasicsJS/dataTypeConv.js
@@ -0,0 +1,16 @@
+
+let number = "Sunny";
+console.log(typeof (number) );
+
+let valInNumber = Number(number);
+console.log(typeof(valInNumber));
+
+//"33" = 33
+//"44abc" = NaN
+//true = 1; false = 0;
+
+
+let isLoggedIn = "Kundan"
+let boolLoggedIn = Boolean(isLoggedIn);
+console.log(typeof(boolLoggedIn));
+
